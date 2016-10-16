2d5200d0722526f15e1a1f6fbfc3da85f4ae7572
anna-cox <anna.cox@mavs.uta.edu>
2016-10-16 16:51:02 -0500

    Updated to include order, associate, and customer

--- View.cpp
+++ View.cpp
@@ -46,10 +46,36 @@ void View::viewBatteries(vector<Battery> list)
 
 void View::viewModels(vector<RobotModel> list)
 {
+	cout << "Models:" << endl;
 	int length = list.size();
 	for (int i = 0; i < length; i++)
 	{
-		cout << "Name: " << list[i].getName() << "\nPrice:" << list[i].getPrice() << "\nDescription:" << list[i].getDescription() << endl;
+		cout << "Name: " << list[i].getName() << "\nPrice:" << list[i].getPrice() << "\nDescription:" << list[i].getDescription() 
+		<<"Model Number: "<< list[i].getModelNumber() << endl;
+	}
+}
+
+void View::viewCustomers(vector<Customer> list)
+{
+
+	int i;
+	int length = list.size();
+	cout << "Customers:" << endl;
+	for (i = 0; i < length; i++)
+	{
+
+		cout << list[i].cust_to_string();
+	}
+}
+
+void View::viewAssociates(vector<SalesAssociate> list)
+{
+	int length = list.size();
+
+	cout << "Sales Associates:" << endl;
+	for (int i = 0; i < length; i++)
+	{
+		cout << list[i].assoc_to_string();
 	}
 }
 
@@ -60,5 +86,8 @@ void View::printMenu()
 		 <<"View Robot Models(1)\n"
 		 <<"Create New Model(2)\n"
 		 <<"Create new Robot Part(3)\n"
-		 <<"Exit(0)\n << endl;
+		 <<"Create Customer (4)\n"
+		 <<"Create Associate (5)\n"
+		 <<"Create Order (6)\n"
+		 <<"Exit(0)\n" << endl;
 }
