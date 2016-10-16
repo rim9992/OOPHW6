4fdeea617be8015267163ca4d93d4969c9993ca8
anna-cox <anna.cox@mavs.uta.edu>
2016-10-16 16:52:49 -0500

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -1,63 +1,53 @@
- #include <iostream>
- #include <string>
-#include <iomanip>
-#include <vector>
-#include<time.h>
 
-#include "RobotModel.h"
-#include "Customer.h"
-#include "SalesAssociate.h"
 #include "Order.h"
+#include "SalesAssociate.h"
+#include "Customer.h"
+
  using namespace std;
 
      void Order::get_date()
      {
            	char today[9];
-            _strdate(today);
+            _strdate_s(today);
             date = today;
      }
 
-    void Order::choose_customer(Customer cust)
+    void Order::choose_customer(int cust)
     {
-        customers = cust.name();
+		customers = cust;
+    }
+    void Order::choose_salesAssoc(int salesAssoc)
+    {
+		salesassocs = salesAssoc;
+    }
+	void Order::choose_RobotModel(RobotModel robo)
+	{
+		robotmodels.push_back(robo.getModelNumber());
+		subtotal += robo.getPrice();
+		weight += robo.getWeight();
+	}
 
-    }
-    void Order::choose_salesAssoc(SalesAssociate salesAssoc)
+    
+	void Order::calculateShipping()
     {
-        salesassocs = salesAssoc.name();
-    }
-    void Order::choose_RobotModel(RobotModel robo)
-    {
-        robotmodels = robo.name();
-    }
-
-    double Order::robotPrice(RobotModel robo)
-    {
-       Robot_Price = robo.price;
-       return Robot_Price;
-    }
-    double Order::calculateShipping()
-    {
-        double weight = RobotModel::totalWeight;
         double cost_per_pound = 1.5;
-        double Shipping = weight * cost_per_pound;
-
-        return Shipping;
-
-
+		shipping = weight * cost_per_pound;
     }
-    double Order::calculateTax()
+
+    void Order::calculateTax()
     {
         double taxes = .0825;
-        Tax = taxes;
-        return Tax;
+        tax = taxes*subtotal;
+    }
 
-    }
-    double Order::totalPrice()
+   double Order::totalPrice()
     {
-        double total_price = Tax + Shipping + Robot_Price;
-        Total = total_price;
-        return Total;
+        total = tax + shipping + Robot_Price;
+		return total;
     }
+   int Order::get_num()
+   {
+	   return OrderNumber;
+   }
 
 
