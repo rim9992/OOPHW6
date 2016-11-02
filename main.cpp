6fd5ae1a28a9e01234e3c8727b0e5a2e614b8677
rim9992 <moralesr2013@gmail.com>
2016-11-01 19:28:05 -0500

    Updated main.cpp to create a window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* Homework 5
+/* Homework 6
  * Robbie Robot Shop
  * 
  * Michael Currie
@@ -6,14 +6,27 @@
  * Rabab Antria
  * Ricardo Morales
  */
-
+#include <stdlib.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 #include "Shop.h"
 
 int main() {
 
-    Shop shop;
-    shop.load();
-    shop.cli();
+   Shop shop;
+   //shop.load(); 
+   //shop.cli();
+	
+	Fl_Window *win = new Fl_Window{640,300,"Robbie Robot Shop"};
+
+	win->position(400,200);
+	win->color(FL_WHITE); 
+	win->resizable(*win);
+	win->end();
+	win->show();
+	return(Fl::run()); 
+
 
 }
 
